e12d9938c3781831c1700c8e2175d3764e9273c0
荀洪道 <kalxd@126.com>
2024-05-26 19:23:30 +0800

    基本配置。

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 import { serve } from "@hono/node-server";
 
 let app = new Hono;
+
+app.use(logger());
 
 app.get("/", c => c.json("hello world"));
 

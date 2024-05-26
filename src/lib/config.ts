9ab29e9a6bb0b32354a2f9deab32d648f017c3e5
荀洪道 <kalxd@126.com>
2024-05-26 19:51:52 +0800

    fix

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 import { ZodType } from "zod";
 
-const curEnv = process.env ?? "development";
+const curEnv = process.env.NODE_ENV ?? "development";
 console.log(curEnv);
 
 export const loadConfig = <T>(codec: ZodType<T>): T => {

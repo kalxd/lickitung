b54c77eda04c7514ce74a78808a19133415c1844
荀洪道 <kalxd@126.com>
2024-05-27 19:58:12 +0800

    最基础的配置读取。

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,59 @@
 import { ZodType } from "zod";
+import { readFileSync, statSync } from "node:fs";
+import * as path from "node:path";
 
 const curEnv = process.env.NODE_ENV ?? "development";
-console.log(curEnv);
+
+const isFileExist = (filepath: string): boolean => {
+	try {
+		statSync(filepath);
+		return true;
+	}
+	catch {
+		return false;
+	}
+};
+
+const concatFileName = (filename: string): string =>
+	path.join(process.cwd(), "config", filename);
+
+const sampleConfigPath = concatFileName("sample.json");
+
+const loadConfigFromPath = (filepath: string): object => {
+	const content = readFileSync(filepath, "utf-8");
+	try {
+		const obj = JSON.parse(content);
+		if (Array.isArray(obj)) {
+			throw new Error(`${filepath}不能是数组`)
+		}
+		else if (typeof obj === "object") {
+			return obj;
+		}
+		else {
+			throw new Error(`${filepath}不是有效的对象`);
+		}
+	}
+	catch (e) {
+		console.error(e);
+		throw new Error(`${filepath}不是有效的配置`);
+	}
+};
+
+const readCurrentConfig = (): object | undefined => {
+	const configPath = concatFileName(`${curEnv}.json`);
+	if (isFileExist(configPath)) {
+		return loadConfigFromPath(configPath);
+	}
+	return undefined;
+};
 
 export const loadConfig = <T>(codec: ZodType<T>): T => {
-	return codec.parse({});
+	const sampleConfig = loadConfigFromPath(sampleConfigPath);
+	const curConfig = readCurrentConfig();
+	const config = {
+		...sampleConfig,
+		...curConfig
+	};
+
+	return codec.parse(config);
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { serve } from "@hono/node-server";
+import { z } from "zod";
+import { loadConfig } from "./lib/config";
+
+const configC = z.object({
+	port: z.number().positive()
+});
+
+const config = loadConfig(configC);
+
+console.log(config);
 
 let app = new Hono;
 
